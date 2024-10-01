6bfa3748f0cc3a7a476ac47a4e5ee4cdf157cf29
Cindy Jiang <ziyijian@andrew.cmu.edu>
2024-10-01 15:11:24 -0400

    exercise finished

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -6,26 +6,32 @@
 
 import Combine
 // import Firebase modules here
-
-
-
+import FirebaseFirestore
 
 class LocationRepository: ObservableObject {
   // Set up properties here
+  private var path: String = "location_scans"
+  private var store = Firestore.firestore()
   
-  
+  @Published var locations: [Location] = []
+  private var cancellables: Set<AnyCancellable> = []
   
   init() {
-    get()
+    self.get()
   }
 
   func get() {
     // Complete this function
-    
-    
-    
-    
-    
-
+    store.collection(path)
+      .addSnapshotListener { querySnapshot, error in
+        if let error = error {
+          print("Error getting locations: \(error.localizedDescription)")
+          return
+        }
+        
+        self.locations = querySnapshot?.documents.compactMap { document in
+          try? document.data(as: Location.self)
+        } ?? []
+      }
   }
 }
